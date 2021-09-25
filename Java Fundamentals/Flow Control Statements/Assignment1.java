9f8558c34de05001046e93f18655e0e2c6e7fc12
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-25 20:02:40 +0530

    Create Assignment1.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment1.java
@@ -0,0 +1,20 @@
+package wipro;
+
+public class training {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = -1;
+		if(num>0) {
+			System.out.println("The number is postive");
+		}
+		else if(num<0) {
+			System.out.println("The number is negative");
+		}
+		else {
+			System.out.println("The number is zero");
+		}
+		
+		
+	}
+}
