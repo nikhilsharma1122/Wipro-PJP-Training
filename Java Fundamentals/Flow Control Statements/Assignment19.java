8249ad6b678baaef83de9dc951db75e4d8aef2bf
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-25 21:42:23 +0530

    Create Assignment19.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment19.java
@@ -0,0 +1,18 @@
+package wipro;
+
+public class training {
+
+	public static void main(String[] args) {
+		
+	int i = 1, count = 0;
+        while (i <= 1000){
+            if (i % 30 == 0){
+                System.out.println(i);
+                count += 1;
+            }
+            if (count == 5)
+                break;
+            i++;
+        }
+    }
+}
