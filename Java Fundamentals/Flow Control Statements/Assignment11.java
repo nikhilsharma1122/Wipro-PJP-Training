41d8852fd0fe36edce28ceaf1495a518443aaadf
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-25 21:00:01 +0530

    Create Assignment11.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment11.java
@@ -0,0 +1,15 @@
+package wipro;
+
+public class training {
+
+	public static void main(String[] args) {
+
+		for (int i = 23; i <= 57; i++) {
+			if(i%2==0) {
+				System.out.print(i);
+			}
+		}
+		
+	}
+
+}
