2681f780ba1a6b7d297eff9eadd9d736e1e603e9
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-25 21:39:20 +0530

    Create Assignment18.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment18.java
@@ -0,0 +1,21 @@
+package wipro;
+
+public class training {
+
+	public static void main(String[] args) {
+		
+		int n = Integer.parseInt(args[0]);
+        int cpn = n;
+        int rev = 0;
+        while(n != 0){
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n /= 10;
+        }
+        if (cpn == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+	}
+	
+}
