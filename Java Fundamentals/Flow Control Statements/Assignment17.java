6f8ac906422da1b30c0d8c957afae17cad1d6587
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-25 21:36:51 +0530

    Create Assignment17.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment17.java
@@ -0,0 +1,17 @@
+package wipro;
+
+public class training {
+
+	public static void main(String[] args) {
+		
+		 int n = Integer.parseInt(args[0]);
+	        int rev = 0;
+	        while(n != 0){
+	            int digit = n % 10;
+	            rev = rev * 10 + digit;
+	            n /= 10;
+	        }
+	        System.out.println(rev);
+	}
+	
+}
