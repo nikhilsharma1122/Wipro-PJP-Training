bccd2942d2be13f0b2b8e7de5958cc98234a4acf
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-25 20:17:37 +0530

    Create Assignment4.java

---
+++ Java Fundamentals/Flow Control Statements/Assignment4.java
@@ -0,0 +1,16 @@
+package wipro;
+
+public class training {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c1 = 's';
+		char c2 = 'e';
+		if(c1<c2) {
+			System.out.println(c1+","+c2);
+		}
+		else {
+			System.out.println(c2+","+c1);
+		}
+	}
+}
