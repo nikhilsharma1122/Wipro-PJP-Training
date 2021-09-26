60ab6aa39c32853ef4eeb3ce89ede09ab5aaff9c
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-26 18:54:53 +0530

    Create Assignment6.java

---
+++ Java Fundamentals/Arrays/Assignment6.java
@@ -0,0 +1,13 @@
+package wipro;
+
+import java.util.Arrays;
+
+public class training {
+
+	public static void main(String[] args) {
+		int[] number = {1,5,4,3,2,6};
+		Arrays.sort(number);
+		System.out.println(Arrays.toString(number));
+		
+	}
+}
