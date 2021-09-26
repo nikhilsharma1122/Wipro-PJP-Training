a1ef0eefac27bc2af3d2b741e8464c314950b394
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-26 18:38:50 +0530

    Create Assignment4.java

---
+++ Java Fundamentals/Arrays/Assignment4.java
@@ -0,0 +1,12 @@
+package wipro;
+
+public class training {
+
+	public static void main(String[] args) {
+		int[] number = {111,114,134,156,117};
+		for(int i = 0;i<number.length;i++) {
+			System.out.printf("%c ", number[i]);
+				
+		}	
+	}
+}
