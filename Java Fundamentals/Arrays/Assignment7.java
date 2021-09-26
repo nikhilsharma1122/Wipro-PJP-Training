f225a81b844495fb1040ff962cc8ea329e29fd86
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-09-26 19:01:58 +0530

    Create Assignment7.java

---
+++ Java Fundamentals/Arrays/Assignment7.java
@@ -0,0 +1,20 @@
+package wipro;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class training {
+
+	public static void main(String[] args) {
+		int[] array = {12, 34, 12, 45, 67, 89};
+		List<Integer> distinctArray = new ArrayList<>();
+		
+		for (int item : array) {
+			if (!distinctArray.contains(item))
+				distinctArray.add(item);
+		}
+		
+		System.out.println(Arrays.toString(distinctArray.toArray()));	
+		
+	}
+}
