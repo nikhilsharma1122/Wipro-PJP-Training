2b355ed3975b44f5145a2ec28d906659298a0a6c
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-05 22:36:06 +0530

    Create Person.java

---
+++ OOPs/Inheritance/Person.java
@@ -0,0 +1,14 @@
+package wipro;
+
+public class Person {
+	String name;
+	 Person(String name){
+		 this.name = name;
+	 }
+	 void setName(String name) {
+		 this.name = name;
+	 }
+	 String getName() {
+		 return this.name;
+	 }
+}
