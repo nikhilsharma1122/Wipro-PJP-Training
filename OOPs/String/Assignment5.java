577e5e46ade8b3dada94f9c8fceb8218ec9d9b06
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-08 09:53:38 +0530

    Create Assignment5.java

---
+++ OOPs/String/Assignment5.java
@@ -0,0 +1,10 @@
+package wipro;
+
+public class testString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0].substring(1, args[0].length() - 1));
+	}
+
+}
