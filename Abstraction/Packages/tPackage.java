dfa935a3a88a8e20015c3f38d2fe970f9f5184ec
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-10 21:57:00 +0530

    Create tPackage.java

---
+++ Abstraction/Packages/tPackage.java
@@ -0,0 +1,9 @@
+package wipro;
+import testPackage.Foundation;
+public class tPackage {
+	public static void main(String[] args) {
+		Foundation fn = new Foundation();
+		fn.var4 = 4;
+		System.out.println(fn.var4);
+	}
+}
