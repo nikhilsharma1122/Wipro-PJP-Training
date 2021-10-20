fb8578bc54bb3ebed31a0f9c8219840c30c30f38
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-20 11:24:55 +0530

    Create Test.java

---
+++ Abstraction/Interfaces/live/Test.java
@@ -0,0 +1,14 @@
+package live;
+import music.string.*;
+import music.wind.*;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Veena v =  new Veena();
+		v.play();
+		Saxophone s = new Saxophone();
+		s.play();
+	}
+
+}
