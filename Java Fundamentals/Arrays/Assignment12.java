e5109a12e04fefde137c426c3b51f536a735c591
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-20 10:10:53 +0530

    Update Assignment12.java

--- Java Fundamentals/Arrays/Assignment12.java
+++ Java Fundamentals/Arrays/Assignment12.java
@@ -9,15 +9,22 @@ public class middleway {
 		for(int i = 0; i<arr1.length;i++) {
 			arr1[i] = scan.nextInt();
 		}
+		int start = 0;
+		int len1 = arr1.length;
+		int mid1 = (start + len1)/2;
+		//System.out.println(arr1[mid]);
+		
 		System.out.println("Enter Array 2 : ");
 		int[] arr2 = new int[3];
 		for(int i = 0; i<arr1.length;i++) {
 			arr2[i] = scan.nextInt();
 		}
+		int len2 = arr2.length;
+		int mid2 = (start + len2)/2;
 		scan.close();
 		int[] arr3 = new int[2];
-		arr3[0] = arr1[1];
-		arr3[1] = arr2[1];
+		arr3[0] = arr1[mid1];
+		arr3[1] = arr2[mid2];
 		System.out.println(arr3[0]+" "+arr3[1]);
 	}
 
