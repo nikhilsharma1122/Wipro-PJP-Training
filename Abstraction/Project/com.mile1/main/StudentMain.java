ba215e299b70a5f0377a6f80dfd44f82c337c042
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-21 17:33:11 +0530

    Update StudentMain.java

--- Abstraction/Project/com.mile1/main/StudentMain.java
+++ Abstraction/Project/com.mile1/main/StudentMain.java
@@ -1,45 +1,66 @@
 package com.mile1.main;
 
 import com.mile1.bean.Student;
-import com.mile1.exception.NullMarksArrayException;
-import com.mile1.exception.NullNameException;
-import com.mile1.exception.NullStudentObjectException;
 import com.mile1.service.StudentReport;
 import com.mile1.service.StudentService;
-
 public class StudentMain {
-	static Student data[] = new Student[4];
-	StudentMain() {
-		for(int i = 0;i<data.length;i++) {
-			data[i]= new Student();
+
+	public static Student data[]= new Student[10];
+	
+	static
+	{
+		for (int i=0;i< data.length;i++)
+		{
+			data[i]=new Student();
 		}
-		data[0] = new Student("Sekar",new int[] {85,75,95,65});
-		data[1] = new Student(null,new int[] {11,22,33});
-		data[2] = null;
-		data[3] = new Student("Manoj",null);
+		data[0]=new Student("Sekar",new int[] {85,75,95});
+		data[1]=new Student(null,new int[] {11,22,33});
+		data[2]=null;
+		data[3]=new Student("Manoj",null);
+		data [4] = new Student("John", new int[] {90, 92, 80}); // TC1
+		data [5] = new Student("Bob", new int[] {35, 40, 50}); // TC2
+		data [6] = null; // TC3
+		data [7] = new Student(null, new int[] {25, 29, 28}); // TC4
+		data [8] = new Student("Arnold", null); // TC5
 	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StudentReport studentreport = new StudentReport();
-		StudentService studentservice = new StudentService();
-		String grade = null;
-		for(Student student:data) {
-			try {
-				grade = studentreport.validate(student);
-			}catch(NullMarksArrayException e) {
-				grade="NullMarksArrayException occured";
-			} catch (NullNameException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NullStudentObjectException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("Grade = "+grade);
-		}
-		System.out.println("Number of objects with Marks array as null = "+studentservice.findNumberOfNullMarksArray(data));
-		System.out.println("Number of objects with Name as null = "+studentservice.findNumberOfNullName(data));
-		System.out.println("Number of objects with Name as null = "+studentservice.findNumberOfNullObjects(data));
+     StudentService s=new StudentService();
+     StudentReport sr=new StudentReport();
+     System.out.println("Grade Calculation:");
+     
+     for (Student i :data)
+     {
+    	 try
+     {
+    	 System.out.println("Grade :"+sr.validate(i));
+    		
+     }
+     catch(Exception e)
+     {
+    	 System.out.println(e);
+     }
+     }
+     
+     
+     System.out.println("null marks :"+s.findNumberOfNullMarks(data));
+     System.out.println("null names :"+s.findNumberOfNullNames(data));
+     System.out.println("null objects :"+s.findNumberOfNullObjects(data));
+     
+     //TC6
+     System.out.println("TC6: Number of Objects with Name as null :" +
+				s.findNumberOfNullNames(data));		
+	// TC7
+	System.out.println("TC7: Number of Objects that are null :" +
+				s.findNumberOfNullObjects(data));
+	// TC8
+	System.out.println("TC8: Number of Objects with Marks array as null :" +
+				s.findNumberOfNullMarks(data));
+     
 	}
+	
 
 }
