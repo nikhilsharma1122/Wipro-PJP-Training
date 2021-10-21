a56bb1812865526bc410e626c6e7c089cdb7de09
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-21 17:32:56 +0530

    Update NullStudentObjectException.java

--- Abstraction/Project/com.mile1/exception/NullStudentObjectException.java
+++ Abstraction/Project/com.mile1/exception/NullStudentObjectException.java
@@ -1,7 +1,14 @@
 package com.mile1.exception;
 
 public class NullStudentObjectException extends Exception{
-	public String toString() {
-		return "NullStudentObjectException occured";
+
+	
+	public String toString()
+	{
+		return "object is null";
 	}
+
+	
+	
+	
 }
