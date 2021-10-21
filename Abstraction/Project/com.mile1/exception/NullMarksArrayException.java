9742475030bc1e029835819ffd1c0474f0f913a6
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-21 17:32:25 +0530

    Update NullMarksArrayException.java

--- Abstraction/Project/com.mile1/exception/NullMarksArrayException.java
+++ Abstraction/Project/com.mile1/exception/NullMarksArrayException.java
@@ -1,7 +1,13 @@
 package com.mile1.exception;
 
 public class NullMarksArrayException extends Exception{
-	public String toString() {
-			return "NullMarksArrayException occured";	
+
+	
+	
+	public String toString()
+	{
+		return " marks array is null";
 	}
+
+	
 }
