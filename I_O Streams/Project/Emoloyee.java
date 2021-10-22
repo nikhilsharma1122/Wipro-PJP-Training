3d1c14fdabd9371899e17445d3fb929a53a3d8f7
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-22 03:49:02 -0700

    Create Emoloyee.java

---
+++ I_O Streams/Project/Emoloyee.java
@@ -0,0 +1,37 @@
+package io.wipro;
+
+public class Employee {
+	int id;
+	String name;
+	int age;
+	double salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return id +" "+ name+" "+age+" "+salary;
+	}
+	
+}
