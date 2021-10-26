962c405e35473a276ed80fea0992f385b7a8a7b2
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-26 03:15:41 -0700

    Create phoneMain.java

---
+++ Collection/Map/phoneMain.java
@@ -0,0 +1,15 @@
+package map;
+import java.util.*;
+public class phoneMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		contactList list = new contactList();
+		 list.addContact("A", 123);
+		 list.addContact("B", 456);
+		 list.addContact("C", 789);
+		 list.getName("B");
+		 list.getNumber(456);
+	}
+
+}
