49d76bb39ed96751287f559830fe07e9d91e794a
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-26 23:03:07 -0700

    Create EmployeeBean.java

---
+++ Collection/Project/com.wipro.bean/EmployeeBean.java
@@ -0,0 +1,44 @@
+package com.wipro.bean;
+
+public class EmployeeBean {
+	private int empID;
+	private String name;
+	private float salary;
+	private String designation;
+	public EmployeeBean(int empID, String name, float salary, String designation) {
+		super();
+		this.empID = empID;
+		this.name = name;
+		this.salary = salary;
+		this.designation = designation;
+		
+	}
+	public void GetEmployeeDetails() {
+        System.out.println("Employee ID = "+empID+"Employee name= "+name+"Employee Salary = "+salary+"Employee Designation: "+designation);
+    }
+	public int getEmpID() {
+		return empID;
+	}
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+}
