114626144c493a582c072b9dd01c9aa8d18e1158
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-10-28 22:18:47 -0700

    Update Assignment 5.sql

--- RDBMS_SQL_JDBC/DML/Assignment 5.sql
+++ RDBMS_SQL_JDBC/DML/Assignment 5.sql
@@ -1,2 +1,2 @@
-insert into my_employee values(203,'Susan','Mavris',40,null);
+insert into my_employee(employee_id primary key,first_name,last_name,department_id,salary) values(203, 'Susan', 'Mavris', 40, null);
 select * from my_employee;
