8d79f8a8865ec58cf56994c4080aacb9312cc3e4
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-11-03 00:41:27 -0700

    Create Conn1.java

---
+++ RDBMS_SQL_JDBC/Establishing-Connection/Conn1.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+public class Conn1 {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","nikhil@123");
+			System.out.println("Connection Successfull");
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Connection could not establish");
+		}finally {
+			if(conn!=null)
+				conn.close();
+		}
+		
+	}
+
+}
