1bc826517900e36ad8aa9d614f7b1386885a1735
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-11-08 00:08:57 -0800

    Create ArrayListLamdaOddString.java

---
+++ Java8/Lambda/ArrayListLamdaOddString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ArrayListLamdaOddString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<StringBuffer> list = new ArrayList<StringBuffer>();
+		String[] names = {"Java", "Python", "C++", "C", "JavaScript", "Go", "Rust", "Bash", "Haskell", "Ruby"};
+		for(String name:names) {
+			StringBuffer sbr = new StringBuffer(name);
+			list.add(sbr);
+		}
+		list.forEach(name->{
+			if(name.length()%2!=0) {
+				System.out.println(name);
+			}
+		});
+	}
+
+}
