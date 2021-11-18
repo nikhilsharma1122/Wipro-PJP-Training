1843526ca5d3fde84150cf9978f86ef9caec2412
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-11-18 22:12:51 +0530

    Create Test.java

---
+++ SpringBasic/StudentTest/src/main/java/com.wipro.bean/Test.java
@@ -0,0 +1,26 @@
+package com.wipro.bean;
+
+public class Test {
+	private String testId;
+	private String testTitle;
+	private int testMarks;
+	public String getTestId() {
+		return testId;
+	}
+	public void setTestId(String testId) {
+		this.testId = testId;
+	}
+	public String getTestTitle() {
+		return testTitle;
+	}
+	public void setTestTitle(String testTitle) {
+		this.testTitle = testTitle;
+	}
+	public int getTestMarks() {
+		return testMarks;
+	}
+	public void setTestMarks(int testMarks) {
+		this.testMarks = testMarks;
+	}
+	
+}
