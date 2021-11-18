d9f0a513300b8ab112ced3ddac30737af937a82f
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-11-18 21:42:28 +0530

    Create SetterMessage.java

---
+++ SpringBasic/SetterMessage/src/main/java/com.wipro.bean/SetterMessage.java
@@ -0,0 +1,15 @@
+package com.wipro.bean
+public class SetterMessage {
+	private String message = null;
+	
+	public SetterMessage() {
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
