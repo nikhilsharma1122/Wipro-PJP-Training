0670afca8eac7a62fbb39ca7ad58aeb9c94d3cd7
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-11-18 15:39:07 +0530

    Create DefaultMessage.java

---
+++ Spring/DefaultMessage/src/main/java/DefaultMessage.java
@@ -0,0 +1,14 @@
+package movie;
+
+public class DefaultMessage {
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
