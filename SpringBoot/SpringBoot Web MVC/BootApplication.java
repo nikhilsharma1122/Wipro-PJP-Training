ea3a6d6978f5b567811f0e6d534f129e3450ff06
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-11-27 11:47:28 +0530

    Create BootApplication.java

---
+++ SpringBoot/SpringBoot Web MVC/BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.BootLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLoginApplication.class, args);
+	}
+
+}
