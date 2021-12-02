4ff9c132477d8ed1754d5c671f773dc851e79bd6
nikhilsharma1122 <69890428+nikhilsharma1122@users.noreply.github.com>
2021-12-02 19:08:23 +0530

    Update EmployeeAdmin.java

--- Hibernate/Introduction To Hibernate/Hibernate_basic_3/src/main/java/com/wipro/service/EmployeeAdmin.java
+++ Hibernate/Introduction To Hibernate/Hibernate_basic_3/src/main/java/com/wipro/service/EmployeeAdmin.java
@@ -12,11 +12,23 @@ public class EmployeeAdmin {
 		SessionFactory sf = cfg.buildSessionFactory();
 		Session session = sf.openSession();
 		Transaction transaction = session.beginTransaction();
-		EmployeeBean emp = new EmployeeBean("Emp1",101,"Desg1",30000);
-		session.save(emp);
+		EmployeeBean emp1 = new EmployeeBean("Emp1",101,"Desg1",30000);
+		EmployeeBean emp2 = new EmployeeBean("Emp2",101,"Desg2",40000);
+		EmployeeBean emp3 = new EmployeeBean("Emp3",101,"Desg3",50000);
+		EmployeeBean emp4 = new EmployeeBean("Emp4",101,"Desg4",60000);
+		EmployeeBean emp5 = new EmployeeBean("Emp5",101,"Desg5",70000);
+		session.save(emp1);
+		session.save(emp2);
+		session.save(emp3);
+		session.save(emp4);
+		session.save(emp5);
 		transaction.commit();
 		System.out.println("Record Inserted");
-		System.out.println(emp);
+		System.out.println(emp1);
+		System.out.println(emp2);
+		System.out.println(emp3);
+		System.out.println(emp4);
+		System.out.println(emp5);
 		session.close();
 		
 	}
